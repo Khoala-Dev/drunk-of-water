c8d4c0103dd90b39415179c2ff2aa44750867139
samatheus <sa.matheusfernandes@gmail.com>
2024-10-28 19:49:22 -0300

    feature(app): Adicionando react-navigation no projeto e configurando arquivos e dependencias

--- App.tsx
+++ App.tsx
@@ -5,12 +5,41 @@ import {
   StyleSheet,
   Text,
   useColorScheme,
-  View,
 } from 'react-native';
-
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-function App(): React.JSX.Element {
+const Tab = createBottomTabNavigator();
+
+const Target = (): React.JSX.Element => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Meta Diária</Text>
+    </SafeAreaView>
+  );
+};
+
+const Progress = (): React.JSX.Element => {
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Progresso</Text>
+    </SafeAreaView>
+  );
+};
+
+const Navigation = (): React.JSX.Element => (
+  <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Meta Diária" component={Target} />
+      <Tab.Screen name="Progresso" component={Progress} />
+    </Tab.Navigator>
+  </NavigationContainer>
+);
+
+export default function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -18,17 +47,13 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <SafeAreaView style={[styles.container, backgroundStyle]}>
+    <SafeAreaProvider>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <View>
-        <Text style={[styles.text, { color: isDarkMode ? '#CCC' :  '#333' }]}>
-          Hello World!
-        </Text>
-      </View>
-    </SafeAreaView>
+      <Navigation />
+    </SafeAreaProvider>
   );
 }
 
@@ -36,12 +61,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
   },
   text: {
     fontSize: 24,
     fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
-
-export default App;
